190d985ab3107e650bdadec95bf6813be38b4e3a
susu04-kor <suin0304_sw@google.com>
2020-04-09 17:12:45 +0900

    블로그 수정삭제

--- bitserver.js
+++ bitserver.js
@@ -1,45 +1,112 @@
 var http = require("http");
 var express = require("express");
 
-var app = express();            //express객체 생성
-app.use(express.static("public"));
-app.use(express.bodyParser());      //사용자로부터 파라메타를 받아드려 처리 할수 있음
-app.use(app.router);            //서버 사용자마다 다르게 하기 위해 router
-
-app.all("/insertblog",function(request, response){
-      var title = request.param("title");
-      var content = request.param("content");
-
-      var doc = {title:title,content:content}
-
-
-      const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
 const assert = require('assert');
-
-// Connection URL
 const url = 'mongodb://localhost:27017';
-// Database name
 const dbName = 'bit';
 
-const client = new MongoClient(url);
+var app = express();
+app.use(express.static("public"));
+app.use(express.bodyParser());
+app.use(app.router);
 
-(async function() {
-  try {
-    await client.connect();
+
+app.get("/blogDetail", function(request, response){
+      var _id = request.param("_id");
+
+      MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db(dbName);
+      dbo.collection("articles").findOne({_id:new ObjectID(_id)}, function(err, result) {
+        if (err) throw err;
+      //  console.log(result);
+        db.close();
+        response.send(result);
+      });
+    });
+});
+
+
+
+app.get("/blogList", function(request, response){
+  const client = new MongoClient(url);
+  client.connect(function(err, client) {
+    assert.equal(null, err);
     console.log("Connected correctly to server");
     const db = client.db(dbName);
+    const col = db.collection('articles');
+        col.find({}).toArray(function(err, docs) {
+        client.close();
+        response.send(docs);
+      });
+  });
+});
 
-    // Insert a single document
-    let r = await db.collection('articles').insertOne(doc);
+app.get("/blogEdit", function(request, response){
+  var _id = request.param("_id");
+  var title = request.param("title");
+  var content = request.param("content");
 
-    // Close connection
+  // var q = {_id:new ObjectID(_id)}
+  var doc = {title:title, content:content}
 
-  } catch(err) {
-    console.log(err.stack);
-  }
-    client.close();
-})();
+  console.log("수정할 문서"+doc);
+  console.log("_id:"+_id);
+  console.log("title:"+title);
+
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db(dbName);
+    var myquery = {_id:new ObjectID(_id)};
+    var newvalues = { $set: doc };
+    dbo.collection("articles").updateOne(myquery, newvalues, function(err, res) {
+      if (err) throw err;
+      console.log("1 document updated");
+      db.close();
+      response.send("1");
+    });
+  });
+});
+
+app.get("/blogDelete", function(request, response){
+  var _id = request.param("_id");
+  var doc = {_id:new ObjectID(_id)};
+
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db(dbName);
+    var myquery = { address: 'Mountain 21' };
+    dbo.collection("articles").deleteOne(doc, function(err, obj) {
+      if (err) throw err;
+      console.log("1 document deleted");
+      db.close();
+      response.send("1");
+    });
+  });
+
 })
+
+
+app.get("/blogInsert", function(request, respone){
+  var title = request.param("title");
+  var content = request.param("content");
+  var saved_at = new Date();
+  var doc = {title:title, content:content, saved_at:saved_at};
+
+    const client = new MongoClient(url);
+    client.connect(function(err, client) {
+    const db = client.db(dbName);
+    db.collection('articles').insertOne(doc, function(err, r) {
+      _id = r.insertedId;
+      client.close();
+      respone.send(_id);
+    });
+  });
+});
+
+
 http.createServer(app).listen(52273, function(){
-    console.log("서버가 가동되었습니다.");
-})
+  console.log("서버가 가동되었습니다");
+});
